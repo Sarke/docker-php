934cfb8318059a789bd75a4e0306df1a973b7a53
Peter Stalman <sarkedev@gmail.com>
2021-06-10 01:09:12 -0700

    powerlevel10k theme to fix docker prompt issue

--- copy/root/setup.zsh
+++ copy/root/setup.zsh
@@ -8,6 +8,8 @@ antigen bundle yii2
 antigen bundle command-not-found
 antigen bundle zsh-users/zsh-syntax-highlighting
 
-antigen theme clean
+antigen theme romkatv/powerlevel10k
 
 antigen apply
+
+source ~/.p10k.zsh
