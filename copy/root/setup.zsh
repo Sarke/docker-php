6d63ac8c56612bd26a72a550cc68b3fad2364f47
Peter Stalman <sarkedev@gmail.com>
2022-01-09 14:34:49 -0800

    antigen bug fix for completions

--- copy/root/setup.zsh
+++ copy/root/setup.zsh
@@ -13,3 +13,7 @@ antigen theme romkatv/powerlevel10k
 antigen apply
 
 source ~/.p10k.zsh
+
+# antigen bug (https://github.com/zsh-users/antigen/issues/698) breaks oh-my-zsh completions, need to be called manually
+#source /root/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/yii2/yii2.plugin.zsh
+# commented out in antigen.zsh instead
