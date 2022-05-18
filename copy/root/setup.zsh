d977e8ac0cf4925e2219f28a46f31b8c5ec1b9de
Peter Stalman <sarkedev@gmail.com>
2022-05-18 11:10:15 -0700

    swap antigen for antibody

--- copy/root/setup.zsh
+++ copy/root/setup.zsh
@@ -1,19 +1,4 @@
 #!/bin/env zsh
-source /root/antigen.zsh
-
-antigen use oh-my-zsh
-
-antigen bundle git
-antigen bundle yii2
-antigen bundle command-not-found
-antigen bundle zsh-users/zsh-syntax-highlighting
-
-antigen theme romkatv/powerlevel10k
-
-antigen apply
+source ~/.zsh_plugins.sh
 
 source ~/.p10k.zsh
-
-# antigen bug (https://github.com/zsh-users/antigen/issues/698) breaks oh-my-zsh completions, need to be called manually
-#source /root/.antigen/bundles/robbyrussell/oh-my-zsh/plugins/yii2/yii2.plugin.zsh
-# commented out in antigen.zsh instead
